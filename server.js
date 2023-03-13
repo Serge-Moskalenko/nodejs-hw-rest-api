a98873b6e3d092509493d3bb96ac23fdfdc2e4ec
Serge-Moskalenko <skmos2019@outlook.com>
2023-03-13 13:36:14 +0000

    fix

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const app = require('./app');
 
 const mongoose = require("mongoose");
-const DB_HOST  = "mongodb+srv://Serhii:eliMiWDsIj4K4YF7@cluster0.oplpleq.mongodb.net/contacts_data?retryWrites=true&w=majority";
+const { DB_HOST }  = process.env;
 
 mongoose.set('strictQuery',true)
 
-mongoose.connect(DB_HOST).then(() => {
-  app.listen(3000, () => {
-    console.log("Server running. Use our API on port: 3000")
-  })
-}).catch(err => {
+mongoose.connect(DB_HOST).then(() => console.log("Server running. Use our API on port: 3000")
+).catch(err => {
   console.log(err.message);
   process.exit(1);
 });
+
+app.listen(3000)
