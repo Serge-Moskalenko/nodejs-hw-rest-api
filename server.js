8252b7cff6e38f868eb330beebeeca5b1e148cce
Serge-Moskalenko <skmos2019@outlook.com>
2023-03-15 11:10:59 +0000

    test

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = require('./app');
 const mongoose = require("mongoose");
 const { DB_HOST }  = process.env;
 
+
 mongoose.set('strictQuery',true)
 
 mongoose.connect(DB_HOST).then(() => {
